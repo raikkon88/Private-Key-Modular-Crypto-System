f2740acea6dea5128abc82aa5ee0dce98fd94b01
Marc Sànchez <msanxes@gmail.com>
2019-10-11 20:13:43 +0200

    [WIP] S'esta generant la documentacio de l'algoritme

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 import auric
+import RailFence
 
-#text = "zfaksjhdflkauehkaetal ukeyhtaksjdhlkathekjryh askdjhajtahsbdafba z z z"
-text = "zazaz"
+text = "zfaksjhdflkauehkaetal ukeyhtaksjdhlkathekjryh askdjhajtahsbdafba z z z"
+#text = "zazaz"
+
+print("Codifiquem : " + text)
 encoded, key = auric.encode(text)
-print(encoded + " -> " + str(key))
-decoded = auric.decode(encoded, key)
-print(decoded)
+print("AURI = " + encoded + " -> " + str(key))
+final = RailFence.codifica(encoded, key % 25)
+print("FENCE = " + final + " -> " + str(key))
+
+decoded = RailFence.descodifica(final, key % 25)
+print("FENCE = " + decoded)
+resultat = auric.decode(decoded, key)
+print("AURI = " + resultat)
